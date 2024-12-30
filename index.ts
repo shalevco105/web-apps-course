660b27cca66899031668e593c51208e18aaa34d6
Rom Bukobza <romrom20022002@gmail.com>
2024-12-30 17:59:11 +0200

    task-2 add swagger

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import swaggerUI from "swagger-ui-express"
+import swaggerJsDoc from "swagger-jsdoc"
 import bodyParser from 'body-parser';
 import postRoute from './src/routes/postRoute';
 import commentsRouter from "./src/routes/commentsRouter";
@@ -15,6 +17,24 @@ app.use(express.json());
 app.use(cors());
 
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV == "development") {
+    const options = {
+        definition: {
+            openapi: "3.0.0",
+            info: {
+                title: "Web Dev Rom&Shalev",
+                version: "1.0.0",
+                description: "REST server Rom and Shalev",
+            },
+            servers: [{ url: "http://localhost:3000" }],
+        },
+        apis: ["src/api/routes/*.ts"],
+    };
+    const specs = swaggerJsDoc(options);
+    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+}
+
 
 app.use("/post", postRoute);
 
